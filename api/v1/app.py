c8b20ca3c6cc7f7aa88067e9608848064da71fbd
Miguel Pacheco <minipachru@gmail.com>
2021-09-19 10:14:52 -0300

    return to previous state

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,28 +2,24 @@
 """
 Contains the FileStorage class
 """
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
-from flask import jsonify
 from flask_cors import CORS
-
-app = Flask(__name__,)
+app = Flask(__name__)
 app.register_blueprint(app_views)
-cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
-
-
-@app.errorhandler(404)
-def not_found(error):
-    """Handles 404 error"""
-    return jsonify({'error': 'Not found'}), 404
-
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 @app.teardown_appcontext
 def teardown_db(exception):
-    """Remove the current SQLAlchemy Session"""
+    """Remove the current SQLAlchemy session"""
     storage.close()
+
+@app.errorhandler(404)
+def not_found(error):
+    """Returns a error 404"""
+    return jsonify({'error': 'Not found'}), 404
 
 if __name__ == "__main__":
     if getenv('HBNB_API_HOST'):
@@ -33,5 +29,5 @@ if __name__ == "__main__":
     if getenv('HBNB_API_PORT'):
         port = getenv('HBNB_API_PORT')
     else:
-        port = '5000'
+        port = 5000
     app.run(host=host, port=port, threaded=True)
